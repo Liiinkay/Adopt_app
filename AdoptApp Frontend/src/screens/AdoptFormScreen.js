d5c66dd7ba7c0e93ecb91dab5832ee5a384c4719
Liiinkay <diegofaraya1@hotmail.com>
2023-11-23 00:55:33 -0300

    se agregan campos faltantes en el formulario de adopción

--- AdoptApp Frontend/src/screens/AdoptFormScreen.js
+++ AdoptApp Frontend/src/screens/AdoptFormScreen.js
@@ -17,6 +17,16 @@ const validationSchema = Yup.object().shape({
   peopleAgreeToAdopt: Yup.boolean().required('Este campo es obligatorio'),
   hasChildren: Yup.boolean().required('Este campo es obligatorio'),
   rentalPermission: Yup.boolean().required('Este campo es obligatorio'),
+  name: Yup.string().required('El nombre es obligatorio'),
+  city: Yup.string().required('La ciudad es obligatoria'),
+  rut: Yup.string()
+    .test(
+    'valid-rut',
+    'El RUT debe de estar escrito con o sin puntos y con guión',
+    (value) => !value || validateRut(value) // Valida solo si hay un valor
+  ),
+  adress: Yup.string().required('La dirección es obligatoria'),
+  phone: Yup.number().typeError('Debe ser un número').required('El teléfono es obligatorio'),
 });
 
 const RadioButton = ({ label, value, onSelect, selectedValue }) => {
@@ -32,6 +42,26 @@ const RadioButton = ({ label, value, onSelect, selectedValue }) => {
   );
 };
 
+const validateRut = (rut) => {
+  let valor = rut.replace(/\./g, '').replace('-', '');
+
+  let cuerpo = valor.slice(0, -1).split('').reverse();
+  let dv = valor.slice(-1).toUpperCase();
+
+  let suma = 0;
+  let multiplo = 2;
+  for (let i = 0; i < cuerpo.length; i++) {
+    suma += parseInt(cuerpo[i]) * multiplo;
+    multiplo = multiplo === 7 ? 2 : multiplo + 1;
+  }
+
+  let dvEsperado = 11 - (suma % 11);
+  dvEsperado = dvEsperado === 11 ? '0' : dvEsperado === 10 ? 'K' : dvEsperado.toString();
+
+  return dv === dvEsperado;
+};
+
+
 const AdoptFormScreen = ({ navigation }) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [submissionMessage, setSubmissionMessage] = useState('');
@@ -42,18 +72,37 @@ const AdoptFormScreen = ({ navigation }) => {
   const handleSubmitForm = async (values) => {
     setIsSubmitting(true);
     try {
-      await createFormAdopt(post.Id, userId, values);
+      const data = transformData(values)
+      console.log(data); 
+      await createFormAdopt(post.id, userId, data);
       setSubmissionMessage('Enviado');
-      setTimeout(() => {
-        // Aquí puedes decidir si deseas limpiar el mensaje o hacer algo más después de un tiempo.
-        setSubmissionMessage('');
-        setIsSubmitting(false);
-      }, 5000); // 5 segundos para mostrar el mensaje de enviado
+      setIsSubmitting(false);
+      navigation.goBack()
     } catch (error) {
       console.error('Error al enviar el formulario:', error);
       setSubmissionMessage('Error al enviar. Intenta nuevamente.');
       setIsSubmitting(false);
     }
+  };
+
+  const transformData = (values) => {
+    const data = {
+      name: values.name,
+      city: values.city,
+      rut: values.rut,
+      adress: values.adress,
+      phone: values.phone,
+      question1: values.reason,
+      question2: values.otherAnimals,
+      question4: values.otherAnimalsSpayed,
+      question5: values.previousAnimals,
+      question6: values.previousAnimalsDescription,
+      question7: values.peopleAgreeToAdopt,
+      question8: values.hasChildren,
+      question9: values.rentalPermission,
+      question10: values.numPeopleInHouse,
+    }
+    return data;
   };
 
   return (
@@ -70,15 +119,20 @@ const AdoptFormScreen = ({ navigation }) => {
       <ScrollView contentContainerStyle={styles.contentContainer}>
         <Formik
           initialValues={{
-            reason: '',
-            otherAnimals: false,
-            otherAnimalsSpayed: false,
-            previousAnimals: '',
-            previousAnimalsDescription: '',
-            numPeopleInHouse: '',
-            peopleAgreeToAdopt: false,
-            hasChildren: false,
-            rentalPermission: false,
+            reason: '',   //Question1
+            otherAnimals: false,    //Question2
+            otherAnimalsSpayed: false,  //Question4
+            previousAnimals: '',      //Question5
+            previousAnimalsDescription: '', //Question6
+            numPeopleInHouse: '',         //Question10
+            peopleAgreeToAdopt: false, //Question7
+            hasChildren: false,       //Question8
+            rentalPermission: false,    //Question9
+            name: '',
+            city: '',
+            rut: '',
+            adress: '',
+            phone: '',
           }}
           validationSchema={validationSchema}
           onSubmit={(values) => { handleSubmitForm(values) }}
@@ -86,12 +140,74 @@ const AdoptFormScreen = ({ navigation }) => {
           {({ handleChange, handleBlur, handleSubmit, values, errors, touched, setFieldValue, isValid, dirty }) => (
             <>
               <Text style={styles.mainTitle}>Formulario de Adopción</Text>
+              <View style={styles.questionContainer}>
+
+                {/* Campo Nombre */}
+                <Text style={styles.inputLabel}>Nombre:</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={handleChange('name')}
+                  onBlur={handleBlur('name')}
+                  value={values.name}
+                  placeholder="Nombre"
+                  maxLength={50}
+                />
+                {touched.name && errors.name && <Text style={styles.errorText}>{errors.name}</Text>}
+
+                {/* Campo Ciudad */}
+                <Text style={styles.inputLabel}>Ciudad:</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={handleChange('city')}
+                  onBlur={handleBlur('city')}
+                  value={values.city}
+                  placeholder="Ciudad"
+                  maxLength={30}
+                />
+                {touched.city && errors.city && <Text style={styles.errorText}>{errors.city}</Text>}
+
+                {/* Campo RUT */}
+                <Text style={styles.inputLabel}>Rut:</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={handleChange('rut')}
+                  onBlur={handleBlur('rut')}
+                  value={values.rut}
+                  placeholder="RUT"
+                  maxLength={25}
+                />
+                {touched.rut && errors.rut && <Text style={styles.errorText}>{errors.rut}</Text>}
+
+                {/* Campo Dirección */}
+                <Text style={styles.inputLabel}>Dirección:</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={handleChange('adress')}
+                  onBlur={handleBlur('adress')}
+                  value={values.adress}
+                  placeholder="Dirección"
+                  maxLength={50}
+                />
+                {touched.adress && errors.adress && <Text style={styles.errorText}>{errors.adress}</Text>}
+
+                {/* Campo Teléfono */}
+                <Text style={styles.inputLabel}>Teléfono:</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={handleChange('phone')}
+                  onBlur={handleBlur('phone')}
+                  value={values.phone}
+                  placeholder="Teléfono"
+                  keyboardType="numeric"
+                  maxLength={9}
+                />
+                {touched.phone && errors.phone && <Text style={styles.errorText}>{errors.phone}</Text>}
+              </View>
 
               {/* Campos del formulario */}
               <View style={styles.questionContainer}>
-
                   {/* 1. Razón para adoptar */}
-                  <Text style={styles.inputLabel}>¿Por qué desea adoptar una mascota?</Text>
+                  <Text style={styles.inputLabel}>1- ¿Por qué desea adoptar una mascota?</Text>
                   <TextInput
                     style={styles.input}
                     onChangeText={handleChange('reason')}
@@ -105,7 +221,7 @@ const AdoptFormScreen = ({ navigation }) => {
 
               {/* 2. Otros animales */}
               <View style={styles.questionContainer}>
-                <Text style={styles.inputLabel}>¿Actualmente cuenta con otros animales?</Text>
+                <Text style={styles.inputLabel}>2- ¿Actualmente cuenta con otros animales?</Text>
                 <RadioButton
                   label="Sí"
                   value={true}
@@ -123,7 +239,7 @@ const AdoptFormScreen = ({ navigation }) => {
 
               {/* 3. Animales esterilizados */}
               <View style={styles.questionContainer}>
-                <Text style={styles.inputLabel}>¿Estos se encuentran esterilizados?</Text>
+                <Text style={styles.inputLabel}>3- ¿Estos se encuentran esterilizados?</Text>
                 <RadioButton
                   label="Sí"
                   value={true}
@@ -141,7 +257,7 @@ const AdoptFormScreen = ({ navigation }) => {
 
               {/* 4. Animales anteriores */}
               <View style={styles.questionContainer}>
-                <Text style={styles.inputLabel}>¿Anteriormente ha tenido otros animales?</Text>
+                <Text style={styles.inputLabel}>4- ¿Anteriormente ha tenido otros animales?</Text>
                 <TextInput
                   style={styles.input}
                   onChangeText={handleChange('previousAnimals')}
@@ -154,7 +270,7 @@ const AdoptFormScreen = ({ navigation }) => {
 
               {/* 5. Descripción de animales anteriores */}
               <View style={styles.questionContainer}>
-                <Text style={styles.inputLabel}>¿Qué fue lo que pasó con ellos?</Text>
+                <Text style={styles.inputLabel}>5- ¿Qué fue lo que pasó con ellos?</Text>
                 <TextInput
                   style={styles.input}
                   onChangeText={handleChange('previousAnimalsDescription')}
@@ -169,7 +285,7 @@ const AdoptFormScreen = ({ navigation }) => {
 
               {/* 6. Número de personas en casa */}
               <View style={styles.questionContainer}>
-                <Text style={styles.inputLabel}>¿Cuántas personas viven en su casa?</Text>
+                <Text style={styles.inputLabel}>6- ¿Cuántas personas viven en su casa?</Text>
                 <TextInput
                   style={styles.input}
                   onChangeText={handleChange('numPeopleInHouse')}
@@ -183,7 +299,7 @@ const AdoptFormScreen = ({ navigation }) => {
 
               {/* 7. Acuerdo para adoptar */}
               <View style={styles.questionContainer}>
-                <Text style={styles.inputLabel}>¿Todos los miembros de la casa están de acuerdo con la adopción?</Text>
+                <Text style={styles.inputLabel}>7- ¿Todos los miembros de la casa están de acuerdo con la adopción?</Text>
                 <RadioButton
                   label="Sí"
                   value={true}
@@ -201,7 +317,7 @@ const AdoptFormScreen = ({ navigation }) => {
 
               {/* 8. Niños en casa */}
               <View style={styles.questionContainer}>
-                <Text style={styles.inputLabel}>¿Hay niños en casa?</Text>
+                <Text style={styles.inputLabel}>8- ¿Hay niños en casa?</Text>
                 <RadioButton
                   label="Sí"
                   value={true}
@@ -219,7 +335,7 @@ const AdoptFormScreen = ({ navigation }) => {
 
               {/* 9. Permiso de alquiler */}
               <View style={styles.questionContainer}>
-                <Text style={styles.inputLabel}>En caso de alquiler, ¿tiene permiso para tener mascotas?</Text>
+                <Text style={styles.inputLabel}>9- En caso de alquiler, ¿tiene permiso para tener mascotas?</Text>
                 <RadioButton
                   label="Sí"
                   value={true}
@@ -306,8 +422,9 @@ const styles = StyleSheet.create({
     overflow: 'hidden', 
   },
   inputLabel: {
-    fontSize: 16,
+    fontSize: 14,
     marginTop: 8,
+    padding: 3,
   },
   input: {
     padding: 15,
