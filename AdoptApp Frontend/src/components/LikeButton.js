ebdf1ea27d984ce7ac7c4867b93c6319a4fa05f6
Luciano Portales <lportales85@gmail.com>
2023-11-23 22:52:38 -0300

    creado boton de like

--- AdoptApp Frontend/src/components/LikeButton.js
+++ AdoptApp Frontend/src/components/LikeButton.js
@@ -1,23 +1,82 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons'; // Asegúrate de importar los iconos de Ionicons desde tu fuente
+import { usePosts } from "../contexts/PostProvider";
+import { Text } from 'react-native';
+import { useAuth } from '../contexts/AuthProvider';
 
-const LikeButton = () => {
-  const [liked, setLiked] = useState(false);
+const LikeButton = ({ postId }) => {
+  const { getUserId } = useAuth();
+  const userId = getUserId();
+  const { getLikesByPostId, likePost, unlikePost } = usePosts();
+  const [isLiked, setIsLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState( 0);
+  const iconName = isLiked ? 'heart' : 'heart-outline'; // Cambia el icono dependiendo del estado
+  const iconColor = isLiked ? 'red' : 'black'; // Cambia el color del icono
 
-  const toggleLike = () => {
-    setLiked(!liked);
+  useEffect(() => {
+    const checkIfLiked = async () => {
+      try {
+        if (!userId) return;
+        if (!postId) return;
+        const likes = await getLikesByPostId(postId);
+        const userLiked = likes.some(like => String(like) === String(userId));
+        setIsLiked(userLiked);
+        setLikesCount(likes.length);
+      } catch (error) {
+        console.log('Error al verificar likes:', error);
+      }
+    };
+
+    checkIfLiked();
+  }, [postId, userId]);
+
+  const handleLike = async () => {
+    console.log('Like button pressed');
+    try {
+      if (isLiked) {
+        console.log('Unlike')
+        await unlikePost(postId, userId);
+        setIsLiked(false);
+        setLikesCount(prev => prev - 1);
+      } else {
+        console.log('Like')
+        await likePost(postId, userId);
+        setIsLiked(true);
+        setLikesCount(prev => prev + 1);
+      }
+    } catch (error) {
+      console.log(error.message);
+      if (error.message.includes('Liked yet')) {
+        console.log('Ya diste like a este post.');
+      } else {
+        console.error('Error al actualizar like:', error);
+      }
+    }
   };
 
   return (
-    <TouchableOpacity onPress={toggleLike}>
-      <Ionicons
-        name={liked ? 'heart' : 'heart-outline'} // Cambia 'heart' y 'heart-outline' por los nombres de tus iconos de Ionicons
-        size={25}
-        color={liked ? '#F348A4' : 'black'} // Cambia 'red' al color que desees para el botón de "like" activo
-      />
+    <TouchableOpacity style={styles.likeButton} onPress={() => {
+      console.log('Like button pressed');
+      handleLike();
+    }}>
+      <Ionicons name={iconName} size={24} color={iconColor} />
+      <Text style={styles.likesCountText}>{likesCount}</Text>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  likeButton: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 10,
+  },
+  likesCountText: {
+      marginLeft: 8,
+      fontSize: 16,
+  },
+});
 
 export default LikeButton;
